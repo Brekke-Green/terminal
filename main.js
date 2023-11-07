bb0fc6459bd45bcf0879ce8812e5557199e17cda
Brekke-Green <brekke.andrew.green@gmail.com>
2023-11-07 13:37:11 -0500

    Build structure for terminal output

--- main.js
+++ main.js
@@ -1,20 +1,29 @@
 import './style.css'
 import { setupCounter } from './counter.js'
 
-let commands = {"help": "", "about": "", "skills": "", "projects": "", "interests": ""}
+let commands = {"help": "", "about": "", "skills": "", "projects": "", "interests": "", "banner": ""}
+let commandsHistory = []
 
 document.querySelector('#app').innerHTML = `
   <div>
     <div id="terminal">
         <div id="history">
-            <div> OUTPUT </div>
+            <div id="terminal-output"></div>
         </div>
         <div id="command-line">
-            <div>visitor@brekkegreen-portfolio:~$</div> <div>█</div>
+            <div id="path">visitor@brekkegreen-portfolio:~$</div>
+<textarea type="text" id="texter" autofocus=""></textarea> <div id="cursor">█</div>
         </div>
         <button id="counter" type="button"></button>
     </div>
   </div>
 `
+
+document.getElementById("cursor").addEventListener("click", () => 
+    document.getElementById("texter").focus())
+
+document.getElementById("texter").addEventListener(() => console.log("Worked"))
+
+document.getElementById("texter").addEventListener(() => document.getElementById("terminal-output").innerHTML = commandsHistory)
 
 setupCounter(document.querySelector('#counter'))
