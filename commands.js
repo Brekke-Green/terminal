b6698f69a86e2b4abd342bd378d378cc4fcc6590
Brekke-Green <brekke.andrew.green@gmail.com>
2023-11-09 12:00:20 -0500

    Build function for runCommand and terminal output

---
+++ commands.js
@@ -0,0 +1,14 @@
+export const commands = {
+    "help": [
+        "<br>",
+        '<span class="command">about</span>          A little about Brekke',
+        '<span class="command">social</span>         Display social networks',
+        '<span class="command">projects</span>       View coding projects',
+        '<span class="command">history</span>        View command history',
+        '<span class="command">help</span>           List of useable commands',
+        '<span class="command">contact</span>        Email/Phone. Please reach out!!',
+        '<span class="command">clear</span>          Clear terminal',
+        '<span class="command">banner</span>         Display the header',
+        "<br>",
+    ],
+}
--- main.js
+++ main.js
@@ -1,25 +1,25 @@
 import './style.css'
 import { setupCounter } from './counter.js'
+import { commands } from './commands.js'
 
 let cursor = document.getElementById("cursor");
 let output = document.getElementById("terminal-output");
 let command = document.getElementById("typer");
 let textarea = document.getElementById("texter");
 
-
-let commands = {"help": "", "about": "", "skills": "", "projects": "", "interests": "", "banner": ""}
 let commandsHistory = []
-
 
 document.querySelector('#app').innerHTML = `
   <div>
     <div id="terminal">
         <div id="history">
-            <div id="terminal-output"></div>
+            <div id="terminal-output">
+                <a id="before"></a> 
+            </div>
         </div>
         <div id="command-line">
-            <div id="path">visitor@brekkegreen-portfolio:~$</div>
-<textarea type="text" id="texter" autofocus=""></textarea><div id="cursor">█</div>
+            <div id="path">visitor@brekkegreen-portfolio:~$ </div>
+<textarea type="text" id="texter" autofocus=""></textarea><span id="typer"></span><div id="cursor">█</div>
         </div>
         <button id="counter" type="button"></button>
     </div>
@@ -29,8 +29,59 @@ document.querySelector('#app').innerHTML = `
 document.getElementById("cursor").addEventListener("click", () => 
     document.getElementById("texter").focus())
 
-document.getElementById("texter").addEventListener("keypress", (e) => {if (e.key === "Enter") { document.getElementById("terminal-output").innerHTML = document.getElementById("texter").value} })
+document.getElementById("texter").addEventListener("keypress", enterPress)
 
 document.getElementById("texter").addEventListener("click", () => {})
+
+function enterPress(e) {
+    if (e.key === "Enter") {
+        commandsHistory.push(document.getElementById("typer").innerHTML)
+        // Print the line to terminal
+        addLine("visitor@brekkegreen-portfolio:~$" + document.getElementById("typer").innerHTML, "no-animation", 0);
+        // run the command
+        runCommand(document.getElementById("typer").innerHTML.toLowerCase());
+        // reset parameters
+        document.getElementById("typer").innerHTML = ""
+        document.getElementById("texter").value = ""
+    } else {
+        document.getElementById("typer").innerHTML = document.getElementById("texter").value
+    }
+}
+
+function runCommand(cmd) {
+    switch (cmd) {
+        case "help":
+            loopLines(commands.help, "", 80);
+            break;
+        default:
+            addLine("<span class=\"inherit\">zsh: command not found: " + cmd + " Type <span class=\"command\">'help'</span> to list available commands.</span>", "error", 100)
+    }
+}
+
+function addLine(text, style, time) {
+    let t = "";
+    for (let i = 0; i < text.length; i++) {
+        if (text.charAt(i) == " " && text.charAt(i+1) == " ") {
+            t += "&nbsp;&nbsp;"
+            i++
+        } else {
+            t += text.charAt(i)
+
+        }
+    }
+    setTimeout(function () {
+        var next = document.createElement("p");
+        next.innerHTML = t;
+        next.className = style;
+        document.getElementById("before").parentNode.insertBefore(next, document.getElementById("before"))
+        window.scrollTo(0, document.body.offsetHeight);
+    }, time)
+}
+
+function loopLines(name, style, time) {
+    name.forEach(function (item, index) {
+        addLine(item, style, index * time);
+    })
+}
 
 setupCounter(document.querySelector('#counter'))
