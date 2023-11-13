a57c6c9eff65321851d9b9da96ffff9be4ab474b
Brekke-Green <brekke.andrew.green@gmail.com>
2023-11-13 09:45:09 -0500

    Update resume link for gh pages

--- commands.js
+++ commands.js
@@ -87,7 +87,7 @@ export const commands = {
     ],
     "resume": [
         "<br>",
-        '<a href="BrekkeAndrewGreenResume.pdf" target="_blank" class="link">Check out my resume here!</a>',
+        '<a href="/terminal/docs/BrekkeAndrewGreenResume.pdf" target="_blank" class="link">Check out my resume here!</a>',
         "<br>",
     ],
 }
